351c50c5bed048b730a833b3b13ee59b341160cd
Bai Junhai <orin@DataResearch01.(none)>
2015-05-08 11:19:29 +0800

    Some word process

--- wordalgo.py
+++ wordalgo.py
@@ -4,11 +4,15 @@ import re
 class discretize:
     #self.signRE = re.compile("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+".decode("utf8"))
     
-    self.signRE = re.compile("[\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+".decode("utf8"))
-    def __init__(self, code='utf8'):
+    def __init__(self, code='utf8', maxchar=6):
         self.code = 'utf8'
+        self.maxchar = 6
+        self.signRE = re.compile("[\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+".decode(self.code))
+        #self.signRE = re.compile("[\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+")
 
-    def removeSign(self, wordline):
-        self.signRE.sub(wordline, ' ')
+    def removePunctuation(self, wordline):
+        return self.signRE.sub(" ".decode(self.code), wordline.decode(self.code))
     def word2char(self, word):
+
+        return False
 
